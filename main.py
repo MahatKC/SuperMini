7e084da3a03af71dbfd30937ca16451e6af21dcc
MahatKC <mkarvat@gmail.com>
2022-07-08 15:36:56 -0300

    main atualizada

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from math import ceil
 
+
 def read_boot(image):
     name = image.read(7)
     blocks_quantity = int.from_bytes(image.read(8), 'little')
@@ -13,42 +14,60 @@ def read_boot(image):
         'bitmap_size' : bitmap_size}
     return dict
 
-def show_folder(image, boot_info, isRoot):
-    next_block = int.from_bytes(image.read(8), 'little')
-    if isRoot:
-        super_block_size = 1
-        size_used = 32
-    else:
-        super_block_size = int.from_bytes(image.read(8), 'little')
-        size_used = 16
-    size_left = boot_info['block_size'] * super_block_size - size_used
+def Read_Folder(image, boot_info, isRoot):
+    next_block = 0
+    while next_block != b'\xFFFFFFFFFFFFFFFF':
+        next_block = int.from_bytes(image.read(8), 'little')
+        if isRoot:
+            super_block_size = 1
+            size_used = 32
+        else:
+            super_block_size = int.from_bytes(image.read(8), 'little')
+            size_used = 16
+        isRoot = False
+        size_left = boot_info['block_size'] * super_block_size - size_used
 
-    mini_block_attribute = image.read(1)
-    while size_left:
-        if mini_block_attribute == b'\x00':
-            break
-        thing_size = int.from_bytes(image.read(7), 'little')
-        first_block = int.from_bytes(image.read(8), 'little')
-        size_left -= 16
-        thing_name = ''
+        
         mini_block_attribute = image.read(1)
-        while mini_block_attribute == b'\x40' and size_left:
-            thing_name += image.read(15).decode('ASCII')
+        folder_content = []
+        while size_left:
+            if mini_block_attribute == b'\x00':
+                break
+            thing_size = int.from_bytes(image.read(7), 'little')
+            first_block = int.from_bytes(image.read(8), 'little')
             size_left -= 16
+            thing_name = ''
+            thing_attribute = mini_block_attribute
             mini_block_attribute = image.read(1)
-        print(thing_name, thing_size, first_block)
+            while mini_block_attribute == b'\x40' and size_left:
+                thing_name += image.read(15).decode('ASCII')
+                size_left -= 16
+                mini_block_attribute = image.read(1)
+            folder_content.append([thing_name, thing_size, first_block, thing_attribute])
+    return folder_content
 
-
-
+def ShowFolder():
     pass
+
+
+def UserInterface():
+    pass
+
+def ShowFile():
+    pass
+
+def WalkImage(image, boot_info, block):
+    image.seek(block * boot_info['block_size'])
+    return
+
 
 
 def main():
     image = open('superMINI.img', 'rb')
     boot_info = read_boot(image)
-    show_folder(image, boot_info, True)
+    Read_Folder(image, boot_info, True)
     image.seek(3*512)
-    show_folder(image, boot_info, False)
+    Read_Folder(image, boot_info, False)
 
 
 
