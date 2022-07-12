fcc6a806dbddea04d080d91a9beb3fcc8099bcb9
MahatKC <mkarvat@gmail.com>
2022-07-12 18:38:36 -0300

    formatação finalizada

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-x = b'\x38'
+from math import ceil
+x='1111100100'
+print(int(x,2).to_bytes(ceil(len(x)/8), byteorder='little'))
 
-print(x[0]//16 == 2)
+y=1
+print(x+'2'*y)
+
+root_miniblock_atribute = 11840
+print(root_miniblock_atribute.to_bytes(16, byteorder='little'))
+
